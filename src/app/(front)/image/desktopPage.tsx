83664f59bb7940b816ad71af42fc2ef4c79a6367
ChengXiaoXiao <gaobo_shengli@outlook.com>
2026-06-27 08:27:38 +0800

    test commit

--- src/app/(front)/image/desktopPage.tsx
+++ src/app/(front)/image/desktopPage.tsx
@@ -23,7 +23,7 @@ export default function DesktopPage({ imageData }: Props) {
   useEffect(() => {
     setTimeout(() => {
       setReady(true);
-    }, 500);
+    }, 250);
   }, []);
 
   const leftButtonHandleClick = (event: any) => {
